04b87f9f7814c73b3f335713128002576eb8def2
kssujithcj <kssujithcj@gmail.com>
2020-09-18 17:43:06 +0530

    corrected login flow documentation link

--- sample/src/Test.java
+++ sample/src/Test.java
@@ -41,10 +41,10 @@ public class Test {
                 }
             });
 
-                /* The request token can to be obtained after completion of login process. Check out https://kite.trade/docs/connect/v1/#login-flow for more information.
-                   A request token is valid for only a couple of minutes and can be used only once. An access token is valid for one whole day. Don't call this method for every app run.
-                   Once an access token is received it should be stored in preferences or database for further usage.
-                 */
+            /* The request token can to be obtained after completion of login process. Check out https://kite.trade/docs/connect/v3/user/#login-flow for more information.
+               A request token is valid for only a couple of minutes and can be used only once. An access token is valid for one whole day. Don't call this method for every app run.
+               Once an access token is received it should be stored in preferences or database for further usage.
+            */
             User user =  kiteConnect.generateSession("xxxxxtttyyy", "xxxxxxxyyyyy");
             kiteConnect.setAccessToken(user.accessToken);
             kiteConnect.setPublicToken(user.publicToken);
