27b8c16490cc011e1732656fa8a4240f96bf2f8b
kssujithcj <kssujithcj@gmail.com>
2021-03-01 13:45:59 +0530

    made all string constants final and added order statuses

--- kiteconnect/src/com/zerodhatech/kiteconnect/utils/Constants.java
+++ kiteconnect/src/com/zerodhatech/kiteconnect/utils/Constants.java
@@ -6,63 +6,72 @@ package com.zerodhatech.kiteconnect.utils;
 public class Constants {
 
     /** Product types. */
-    public static String PRODUCT_MIS = "MIS";
-    public static String PRODUCT_CNC = "CNC";
-    public static String PRODUCT_NRML = "NRML";
+    public static final String PRODUCT_MIS = "MIS";
+    public static final String PRODUCT_CNC = "CNC";
+    public static final String PRODUCT_NRML = "NRML";
 
     /** Order types. */
-    public static String ORDER_TYPE_MARKET = "MARKET";
-    public static String ORDER_TYPE_LIMIT = "LIMIT";
-    public static String ORDER_TYPE_SL = "SL";
-    public static String ORDER_TYPE_SLM = "SL-M";
+    public static final String ORDER_TYPE_MARKET = "MARKET";
+    public static final String ORDER_TYPE_LIMIT = "LIMIT";
+    public static final String ORDER_TYPE_SL = "SL";
+    public static final String ORDER_TYPE_SLM = "SL-M";
 
     /** Variety types. */
-    public static String VARIETY_REGULAR = "regular";
-    public static String VARIETY_BO = "bo";
-    public static String VARIETY_CO = "co";
-    public static String VARIETY_AMO = "amo";
+    public static final String VARIETY_REGULAR = "regular";
+    public static final String VARIETY_BO = "bo";
+    public static final String VARIETY_CO = "co";
+    public static final String VARIETY_AMO = "amo";
 
     /** Transaction types. */
-    public static String TRANSACTION_TYPE_BUY = "BUY";
-    public static String TRANSACTION_TYPE_SELL = "SELL";
+    public static final String TRANSACTION_TYPE_BUY = "BUY";
+    public static final String TRANSACTION_TYPE_SELL = "SELL";
 
     /** Position types. */
-    public static String POSITION_DAY = "day";
-    public static String POSITION_OVERNIGHT = "overnight";
+    public static final String POSITION_DAY = "day";
+    public static final String POSITION_OVERNIGHT = "overnight";
 
     /** Validity types. */
-    public static String VALIDITY_DAY = "DAY";
-    public static String VALIDITY_IOC = "IOC";
+    public static final String VALIDITY_DAY = "DAY";
+    public static final String VALIDITY_IOC = "IOC";
 
     /** Exchanges. */
-    public static String EXCHANGE_NSE = "NSE";
-    public static String EXCHANGE_BSE = "BSE";
-    public static String EXCHANGE_NFO = "NFO";
-    public static String EXCHANGE_BFO = "BFO";
-    public static String EXCHANGE_MCX = "MCX";
-    public static String EXCHANGE_CDS = "CDS";
+    public static final String EXCHANGE_NSE = "NSE";
+    public static final String EXCHANGE_BSE = "BSE";
+    public static final String EXCHANGE_NFO = "NFO";
+    public static final String EXCHANGE_BFO = "BFO";
+    public static final String EXCHANGE_MCX = "MCX";
+    public static final String EXCHANGE_CDS = "CDS";
 
     /** Margin segments. */
-    public static String MARGIN_EQUITY = "equity";
-    public static String MARGIN_COMMODITY = "commodity";
+    public static final String MARGIN_EQUITY = "equity";
+    public static final String MARGIN_COMMODITY = "commodity";
 
     /** Instruments segments. */
-    public static String INSTRUMENTS_SEGMENTS_EQUITY = "equity";
-    public static String INSTRUMENTS_SEGMENTS_COMMODITY = "commodity";
-    public static String INSTRUMENTS_SEGMENTS_FUTURES = "futures";
-    public static String INSTRUMENTS_SEGMENTS_CURRENCY = "currency";
+    public static final String INSTRUMENTS_SEGMENTS_EQUITY = "equity";
+    public static final String INSTRUMENTS_SEGMENTS_COMMODITY = "commodity";
+    public static final String INSTRUMENTS_SEGMENTS_FUTURES = "futures";
+    public static final String INSTRUMENTS_SEGMENTS_CURRENCY = "currency";
 
     /* GTT status */
-    public static String ACTIVE = "active";
-    public static String TRIGGERED = "triggered";
-    public static String DISABLED = "disabled";
-    public static String EXPIRED = "expired";
-    public static String CANCELLED = "cancelled";
-    public static String REJECTED = "rejected";
-    public static String DELETED = "deleted";
+    public static final String ACTIVE = "active";
+    public static final String TRIGGERED = "triggered";
+    public static final String DISABLED = "disabled";
+    public static final String EXPIRED = "expired";
+    public static final String CANCELLED = "cancelled";
+    public static final String REJECTED = "rejected";
+    public static final String DELETED = "deleted";
 
 
     /* GTT trigger type */
-    public static String OCO = "two-leg";
-    public static String SINGLE = "single";
+    public static final String OCO = "two-leg";
+    public static final String SINGLE = "single";
+
+    /*These are commonly used order statuses but there are many other order statuses.
+    * Some of the statuses are only intermediate. */
+    public static final String ORDER_CANCELLED = "CANCELLED";
+    public static final String ORDER_REJECTED = "REJECTED";
+    public static final String ORDER_COMPLETE = "COMPLETE";
+    public static final String ORDER_OPEN = "OPEN";
+    public static final String ORDER_LAPSED = "LAPSED";
+    public static final String ORDER_TRIGGER_PENDING = "TRIGGER PENDING";
 }
